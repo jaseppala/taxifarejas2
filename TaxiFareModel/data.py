5b565a376f9f26db8d6aef8c26c2bd71f8267ab0
Jonas Seppala <jonas.seppala@gmail.com>
2021-08-17 12:55:11 +0200

    improved

--- TaxiFareModel/data.py
+++ TaxiFareModel/data.py
@@ -3,7 +3,7 @@ import pandas as pd
 AWS_BUCKET_PATH = "s3://wagon-public-datasets/taxi-fare-train.csv"
 
 
-def get_data(nrows=100):
+def get_data(nrows=10_000):
     '''returns a DataFrame with nrows from s3 bucket'''
     df = pd.read_csv(AWS_BUCKET_PATH, nrows=nrows)
     return df
